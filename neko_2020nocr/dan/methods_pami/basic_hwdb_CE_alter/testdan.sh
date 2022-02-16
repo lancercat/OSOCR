a32d6d99dc80694c69d068a04a0048583d2a74dc
lasercat <lasercat@gmx.us>
2022-02-16 16:52:03 +0800

    [v2] Bug fixes

--- neko_2020nocr/dan/methods_pami/basic_hwdb_CE_alter/testdan.sh
+++
@@ -1,6 +0,0 @@
-export PYTHONPATH=../../../../
-export CUDA_VISIBLE_DEVICES=$1
-python test.py 20 &> TESTDAN20.log
-python test.py 15 &> TESTDAN15.log
-python test.py 10 &> TESTDAN10.log
-python test.py 5 &> TESTDAN5.log
