8085ccf2554d61c68bf8958f053233eba187d01d
lasercat <lasercat@gmx.us>
2022-02-17 12:25:07 +0800

    [v2] Bug fixes

--- neko_2020nocr/dan/methods_pami/basft_chsHSCQA_CE_alter/test-jap.py
+++ neko_2020nocr/dan/methods_pami/basft_chsHSCQA_CE_alter/test-jap.py
@@ -9,9 +9,9 @@ from neko_2020nocr.dan.danframework.HEXOScvpr21 import HDOS2C;
 #---------------------------------------------------------
 if __name__ == '__main__':
     root="/run/media/lasercat/ssddata/chs-japxl"
-    cfgs=scene_cfg_tejp()
+    cfgs=scene_cfg_tejp(root_override="/run/media/lasercat/ssddata/pamidump/prfinal/ablative/")
     runner=HDOS2C(cfgs);
-    runner.run("/run/media/lasercat/ssddata/pamiremake/basict_chjap/");
+    runner.run();
 
 
 
